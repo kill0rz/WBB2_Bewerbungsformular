b45200552b078aa84911abd04f7792561757c58b
kill0rz <kill0rz@users.noreply.github.com>
2017-01-02 18:49:53 +0100

    + list current fields + added licence + updated database

--- wbb2/acp/bewerbungsformular.sql
+++ wbb2/acp/bewerbungsformular.sql
@@ -4,15 +4,15 @@ CREATE TABLE `bb1_bewerbungsformular_fieldtypes` (
   `typename` varchar(20) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `bb1_bewerbungsformular_fields`;
-
-
 CREATE TABLE `bb1_bewerbungsformular_fields` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `page` int(11) NOT NULL,
   `fieldtype` int(11) NOT NULL,
-  `fieldtext` varchar(500) NOT NULL,
-  `fildcontent` varchar(500) NOT NULL,
+  `fieldname` varchar(500) NOT NULL,
+  `fieldcontent` varchar(500) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `fieldtype` (`fieldtype`),
   CONSTRAINT `bb1_bewerbungsformular_fields_ibfk_2` FOREIGN KEY (`fieldtype`) REFERENCES `bb1_bewerbungsformular_fieldtypes` (`ID`)
--- wbb2/acp/bewerbungsformular_admin.php
+++ wbb2/acp/bewerbungsformular_admin.php
@@ -1,13 +1,12 @@
 <?php
 
 /* Bewerbungsformular v1.0 by kill0rz (C) 2017 */
-/* for WBB 2.3.6 /*
+/* for WBB 2.3.6 */
 /* kill0rz.com */
 
 require './global.php';
 require './lib/class_parse.php';
 require './lib/class_parsecode.php';
-require './wm2018_gameids.php';
 
 if (!checkAdminPermissions("a_can_bewerbungsformular_edit")) {
 	access_error(1);
@@ -21,12 +20,36 @@ if (isset($_REQUEST['action'])) {
 	$action = "info";
 }
 
+function get_next_page() {
+	global $db, $n;
+	$sql = "SELECT page FROM bb" . $n . "_bewerbungsformular_fields GROUP BY page ORDER BY page;";
+	$result = $db->query($sql);
+	$pagecount = 0;
+	while ($row = $db->fetch_array($result)) {
+		if ($pagecount + 1 == $row['page']) {
+			$pagecount++;
+		} else {
+			break;
+		}
+	}
+	return $pagecount + 1;
+}
+
 switch ($action) {
-	case 'info':
-		eval("\$tpl->output(\"" . $tpl->get('bewerbungsformular_info', 1) . "\");");
+	case 'options':
+		$count = 0;
+		$bewerbungsformular_options_bit_var = '';
+		$sql = "SELECT * FROM bb" . $n . "_bewerbungsformular_fields ORDER BY page ASC, fieldtype ASC, fieldname ASC;";
+		$result = $db->query($sql);
+		while ($row = $db->fetch_array($result)) {
+			$rowclass = getone($count++, "firstrow", "secondrow");
+			eval("\$bewerbungsformular_options_bit_var .= \"" . $tpl->get('bewerbungsformular_options_bit', 1) . "\";");
+		}
+
+		eval("\$tpl->output(\"" . $tpl->get('bewerbungsformular_options', 1) . "\");");
 		break;
 
 	default:
-		# code...
+		eval("\$tpl->output(\"" . $tpl->get('bewerbungsformular_info', 1) . "\");");
 		break;
 }
