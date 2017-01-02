faf9c6e13fefaef8d975e5f4348ed4ae771aa043
kill0rz <kill0rz@users.noreply.github.com>
2017-01-02 17:03:45 +0100

    database init

---
+++ wbb2/acp/bewerbungsformular.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS `bb1_bewerbungsformular_fieldtypes`;
+CREATE TABLE `bb1_bewerbungsformular_fieldtypes` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `typename` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `bb1_bewerbungsformular_fields`;
+
+
+CREATE TABLE `bb1_bewerbungsformular_fields` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `page` int(11) NOT NULL,
+  `fieldtype` int(11) NOT NULL,
+  `fieldtext` varchar(500) NOT NULL,
+  `fildcontent` varchar(500) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fieldtype` (`fieldtype`),
+  CONSTRAINT `bb1_bewerbungsformular_fields_ibfk_2` FOREIGN KEY (`fieldtype`) REFERENCES `bb1_bewerbungsformular_fieldtypes` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
